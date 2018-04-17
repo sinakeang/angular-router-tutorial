fbff3d74f8dc7ae8dc279a76266e4b4df01fbc0c
Sina Keang <sina.keang@innovapost.com>
2018-04-16 20:10:38 -0400

    using routerLink programatically from typescript with navigate()

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  // inject Router if we need to use it
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  onLoadServers() {
+    //assume we are doing complex calculation
+    //navigate allows us to use routerLink programatically
+    //using absolute servers path with prefix /
+    this.router.navigate(['/servers']);
+  }
 }
