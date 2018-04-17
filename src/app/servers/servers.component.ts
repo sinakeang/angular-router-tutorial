4c43fce24a2a0e311730e42ac2e82cb689b289fe
Sina Keang <sina.keang@innovapost.com>
2018-04-16 21:21:34 -0400

    programatically use router.navigate without / with relativeTo

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -9,10 +10,28 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   private servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  // ActivatedRoute injects the currently activated route
+  constructor(private serversService: ServersService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload() {
+    // User relative path but this time there's no error since
+    // unlike the navigate link, the router link does not know the link you are concurrently on?
+    // the router link always knows in which component it seats?
+    // by default relativeTo uses the root domain
+    // relativeTo: this.route let NG know what our currently activated route is
+    // currently this will break the link as NG assume we want this route
+    // which is /servers, then we navigate to servers which becomes /servers/servers resulting
+    // in error.
+    // The point here is to show how one would create a relative route base this component.
+    // this.router.navigate(['servers'], {relativeTo: this.route}); keep for reference
+
+    
   }
 
 }
